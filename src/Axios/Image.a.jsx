54db1d1e9a2667f775119bbc7d00f1c9335c3fd8
20120474 <84512387+kimhieu153255@users.noreply.github.com>
2023-11-04 13:34:30 +0700

    change URL BE

--- src/Axios/Image.a.jsx
+++ src/Axios/Image.a.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const imageAxiosInstance = axios.create({
-  // baseURL: "https://shop-demo1.onrender.com/",
-  baseURL: "http://localhost:20474/",
+  baseURL: "https://shop-demo1.onrender.com/",
+  // baseURL: "http://localhost:20474/",
   headers: {
     "Content-Type": "multipart/form-data",
     Authorization: "Bearer " + Cookies.get("token"),
--- src/Axios/NonToken.a.jsx
+++ src/Axios/NonToken.a.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const nonTokenAxiosInstance = axios.create({
-  // baseURL: "https://shop-demo1.onrender.com/",
-  baseURL: "http://localhost:20474/",
+  baseURL: "https://shop-demo1.onrender.com/",
+  // baseURL: "http://localhost:20474/",
 });
 
 export default nonTokenAxiosInstance;
